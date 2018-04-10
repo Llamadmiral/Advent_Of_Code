db3b9edcb388df07e3017899097e7b8d553d1938
Llamadmiral <adam.maczak@loxon.eu>
2018-04-10 17:59:02 +0200

    Solved day 19, altough not entirely... learned something atleast!

--- Year2016/src/com/aoc/days2016/day19/SolutionNineteen.java
+++ Year2016/src/com/aoc/days2016/day19/SolutionNineteen.java
@@ -6,17 +6,34 @@ import com.aoc.solutionbase.SolutionBase;
  * @author Llamadmiral.
  */
 class SolutionNineteen extends SolutionBase {
+
+    private static final int STARTING_SIZE = 3005290;
+
     SolutionNineteen(final String day) {
         super(day);
     }
 
     @Override
     protected void solvePartOne() {
-        //not yet solved
+        int i = 1;
+        while (i * 2 < STARTING_SIZE) {
+            i *= 2;
+        }
+        setSolutionOne((STARTING_SIZE - i) * 2 + 1);
     }
 
+    /**
+     * Not my code... I couldn't solve it in a reasonable time. :(
+     */
     @Override
     protected void solvePartTwo() {
-        //not yet solved
+        int winner = 1;
+        for (int i = 1; i < STARTING_SIZE; i++) {
+            winner = winner % i + 1;
+            if (winner > (i + 1) / 2) {
+                winner++;
+            }
+        }
+        setSolutionTwo(winner);
     }
 }
