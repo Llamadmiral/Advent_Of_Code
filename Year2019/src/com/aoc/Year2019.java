cfb84b43a5ce734f9548106121dcdcf384d591b3
Adam Maczak <Adam.Maczak@loxon.eu>
2020-12-07 10:47:09 +0100

    This is a year old commit lol

---
+++ Year2019/src/com/aoc/Year2019.java
@@ -0,0 +1,47 @@
+package com.aoc;
+
+
+import com.aoc.yearbase.YearBase;
+
+/**
+ * @author Llamadmiral.
+ */
+public class Year2019 extends YearBase {
+
+    public Year2019() {
+        this.year = 2019;
+    }
+
+    @Override
+    public void testDay() {
+    }
+
+    @Override
+    protected void printDays() {
+        /*printDay(new DayOne());
+        printDay(new DayTwo());
+        printDay(new DayThree());
+        printDay(new DayFour());
+        printDay(new DayFive());
+        printDay(new DaySix());
+        printDay(new DaySeven());
+        printDay(new DayEight());
+        printDay(new DayNine());
+        printDay(new DayTen());
+        printDay(new DayEleven());
+        printDay(new DayTwelve());
+        printDay(new DayThirteen());
+        printDay(new DayFourteen());
+        printDay(new DayFifteen());
+        printDay(new DaySixteen());
+        printDay(new DaySeventeen());
+        printDay(new DayEighteen());
+        printDay(new DayNineteen());
+        printDay(new DayTwenty());
+        printDay(new DayTwentyOne());
+        printDay(new DayTwentyTwo());
+        printDay(new DayTwentyThree());
+        printDay(new DayTwentyFour());
+        printDay(new DayTwentyFive());*/
+    }
+}
