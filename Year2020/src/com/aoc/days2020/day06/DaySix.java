c7afd74c8d6516c84833ea23f102af2def89a085
Maczák Ádám <Adam.Maczak@loxon.eu>
2020-12-13 13:29:43 +0100

    Solved 2020 day 6!

--- Year2020/src/com/aoc/days2020/day06/DaySix.java
+++ Year2020/src/com/aoc/days2020/day06/DaySix.java
@@ -14,7 +14,7 @@ public class DaySix extends DayBase {
     public DaySix() {
         dayNr = DAY_NR;
         solution = new SolutionSix(DAY_NR);
-        //solution.setInput(Reader.readFromInput("2020/6.txt"));
+        solution.setInput(Reader.readFromInput("2020/6.txt"));
     }
 
     @Override
@@ -24,6 +24,20 @@ public class DaySix extends DayBase {
 
     @Override
     public String[] getTestInputs() {
-        return new String[]{};
+        return new String[]{"abc\n"
+            + "\n"
+            + "a\n"
+            + "b\n"
+            + "c\n"
+            + "\n"
+            + "ab\n"
+            + "ac\n"
+            + "\n"
+            + "a\n"
+            + "a\n"
+            + "a\n"
+            + "a\n"
+            + "\n"
+            + "b"};
     }
 }
--- Year2020/src/com/aoc/days2020/day06/SolutionSix.java
+++ Year2020/src/com/aoc/days2020/day06/SolutionSix.java
@@ -2,21 +2,74 @@ package com.aoc.days2020.day06;
 
 import com.aoc.solutionbase.SolutionBase;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Llamadmiral.
  */
 class SolutionSix extends SolutionBase {
+
+    private List<String[]> groups = new ArrayList<>();
+
     SolutionSix(final String day) {
         super(day);
     }
 
     @Override
+    public void init() {
+        input = input.replaceAll("\n\n", "\t").replaceAll("\n", " ").replaceAll("\t", "\n");
+        final String[] rows = input.split("\n");
+        for (final String row : rows) {
+            final String[] group = row.split(" ");
+            this.groups.add(group);
+        }
+    }
+
+    @Override
     protected void solvePartOne() {
-        //not yet solved
+        int sum = 0;
+        for (final String[] group : groups) {
+            final Set<Character> uniqueAnswers = new HashSet<>();
+            for (final String person : group) {
+                for (int i = 0; i < person.length(); i++) {
+                    uniqueAnswers.add(person.charAt(i));
+                }
+            }
+            sum += uniqueAnswers.size();
+        }
+        setSolutionOne(sum);
     }
 
     @Override
     protected void solvePartTwo() {
-        //not yet solved
+        int count = 0;
+        for (final String[] group : groups) {
+            for (int i = 0; i < group[0].length(); i++) {
+                final char question = group[0].charAt(i);
+                boolean questionAsnweredByAll = true;
+                for (int j = 1; j < group.length; j++) {
+                    if (group[j].indexOf(question) == -1) {
+                        questionAsnweredByAll = false;
+                    }
+                }
+                if (questionAsnweredByAll) {
+                    count++;
+                }
+            }
+        }
+        setSolutionTwo(count);
+    }
+
+    private int countCharacters(final String group, final char c) {
+        int count = 0;
+        for (int i = 0; i < group.length(); i++) {
+            if (group.charAt(i) == c) {
+                count++;
+            }
+        }
+        return count;
     }
 }
